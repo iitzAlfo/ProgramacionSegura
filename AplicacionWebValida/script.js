d0fe24c1f95b54f410b3fbb99a47ed8e2e24582c
Alfonso Garcia Salas <90407659+iitzAlfo@users.noreply.github.com>
2022-03-31 11:57:22 -0600

    Add files via upload

---
+++ AplicacionWebValida/script.js
@@ -0,0 +1,18 @@
+function curpValida() {
+    var curp =document.getElementById("curp").value;
+    var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/
+	if(curp.match(re))
+        alert("Si es una Curp")
+    else
+        alert("No es una Curp")
+  
+}
+function mailvalido(){
+    var mail = document.getElementById("mail").value;
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if(mail.match(expReg))
+        alert("Es un Email valido")
+    else
+        alert("El Mail no es valido")
+    
+}
